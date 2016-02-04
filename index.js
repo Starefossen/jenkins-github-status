7a839e6b5435f808233c0f8a14aad914d9817625
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-02-04 19:40:06 +0100

    feat(cron): add initial cron routine

---
+++ index.js
@@ -0,0 +1,33 @@
+/* eslint no-console: 0 */
+'use strict';
+
+const jenkins = require('./lib/jenkins');
+const schedule = require('node-schedule');
+const redis = require('./lib/redis');
+const irc = require('./lib/irc');
+
+schedule.scheduleJob('* * * * *', () => {
+  console.log('[CRON] running');
+
+  jenkins.getBuilds({ limit: 5 }, (b, next) => {
+    const key = `build:${b.id}`;
+
+    redis.exists(key, (existsErr, exists) => {
+      if (existsErr) { return next(existsErr); }
+      if (exists === 1) { return next(); }
+
+      console.log('[CRON]', key, 'NEW');
+
+      redis.hmset(key, b, setErr => {
+        if (setErr) { return next(setErr); }
+        irc.notify(b, next);
+      });
+    });
+  }, jenkinsErr => {
+    if (jenkinsErr) { throw jenkinsErr; }
+
+    console.log('[CRON] done');
+  });
+});
+
+process.on('SIGINT', process.exit.bind(process, 0));
